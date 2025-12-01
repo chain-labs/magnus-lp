aa3fc9cd22458f349b4b2ba600ed9c60ee01fd16
Pratham <chudasamapratham142@gmail.com>
2025-12-01 17:59:44 +0530

    Add InvestmentPhilosophy component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BookACall from "@/components/BookACall";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
+import InvestmentPhilosophy from "@/components/InvestmentPhilosophy";
 import MeetTheFounder from "@/components/MeetTheFounder";
 import OurTrackRecord from "@/components/OurTrackRecord";
 import WhatOurClientsSay from "@/components/WhatOurClientsSay";
@@ -10,6 +11,7 @@ export default function Home() {
 	return (
 		<>
 			<YoureStuckInAsystemDesignedToWorkAgainstYou />
+			<InvestmentPhilosophy />
 			<OurTrackRecord />
 			<WhatOurClientsSay />
 			<FAQ />
---
+++ components/InvestmentPhilosophy.tsx
@@ -0,0 +1,11 @@
+export default function InvestmentPhilosophy() {
+	return (
+		<section className="w-full py-[120px] px-[80px]">
+			<div className="max-w-7xl mx-auto flex flex-col gap-[80px]">
+				<h2 className="max-w-2xl text-[40px] leading-[48px]">
+					Investment Philosophy
+				</h2>
+			</div>
+		</section>
+	);
+}
