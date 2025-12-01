ad0ac80472163f066680b6f68d0086893956e03a
Pratham <chudasamapratham142@gmail.com>
2025-12-01 14:34:21 +0530

    Add OurTrackRecord component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
+import OurTrackRecord from "@/components/OurTrackRecord";
 import WhatOurClientsSay from "@/components/WhatOurClientsSay";
 
 export default function Home() {
 	return (
 		<>
+			<OurTrackRecord />
 			<WhatOurClientsSay />
 			<FAQ />
 			<Footer />
---
+++ components/OurTrackRecord.tsx
@@ -0,0 +1,55 @@
+const trackRecordData = [
+	{
+		value: "4,000+",
+		description: "Retail Investors Trust Our Research",
+	},
+	{
+		value: "25+",
+		description: "Years Cumulative Team Experience",
+	},
+	{
+		value: "500k+",
+		description: "Social Media Following",
+	},
+	{
+		value: "High",
+		description: "Renewal Rate Through Oct 2024— Apr 2025 Crash",
+	},
+];
+
+export default function OurTrackRecord() {
+	return (
+		<section className="w-full bg-[#000728] py-[112px] px-[80px]">
+			<div className="max-w-7xl mx-auto">
+				<div className="grid grid-cols-2 gap-[80px]">
+                    <div className="w-full h-full bg-[#67676733] rounded-[12px]"></div>
+					<div className="flex flex-col gap-[24px]">
+						<div className="flex flex-col gap-[8px]">
+							<h2 className="text-[40px] leading-[48px] text-white">
+								Our track record
+							</h2>
+							<p className="text-[16px] leading-[24px] text-white">
+								We don't promise returns. We promise clarity.
+							</p>
+						</div>
+						<div className="grid grid-cols-2 gap-[24px]">
+							{trackRecordData.map((item, index) => (
+								<div
+									key={index}
+									className="rounded-[8px] bg-[#F8F9FB1A] py-[16px] px-[12px] flex flex-col gap-4"
+								>
+									<div className="text-[32px] leading-[40px] text-white">
+										{item.value}
+									</div>
+									<p className="text-[16px] leading-[24px] text-white opacity-60">
+										{item.description}
+									</p>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
