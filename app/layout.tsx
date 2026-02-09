909d2e5744ff0e7eb9426c284ed7e9c2f1103359
Pratham <chudasamapratham142@gmail.com>
2026-02-09 19:21:07 +0530

    style: improve formatting and indentation of Google Tag Manager integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,22 +96,28 @@ export default function RootLayout({
 				></link>
 
 				<Script
-			id="gtm-tags"
-			strategy="afterInteractive"
-			dangerouslySetInnerHTML={{
-				__html: `
+					id="gtm-tags"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
                     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PL6D4ZPL');
         `,
-			}}
-		/>
+					}}
+				/>
 			</head>
-			<body className={'antialiased'}>
-				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PL6D4ZPL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			<body className={"antialiased"}>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-PL6D4ZPL"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					></iframe>
+				</noscript>
 				<Clarity />
 				<SanityLive />
 				{children}
