d456c7fc2f799c1243f079a8c427c79710066247
Pratham <52632050+PRATHAM1ST@users.noreply.github.com>
2026-02-09 13:03:36 +0530

    Integrate Google Tag Manager into layout
    Added Google Tag Manager script and noscript iframe for tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Clarity from "@/components/Clarity";
 import { SanityLive } from "@/sanity/lib/live";
+import Script from "next/script";
+
 const helvetica = localFont({
 	src: "../public/assets/fonts/Helvetica.ttf",
 	variable: "--font-helvetica",
@@ -92,8 +94,24 @@ export default function RootLayout({
 					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 					rel="stylesheet"
 				></link>
+
+				<Script
+			id="gtm-tags"
+			strategy="afterInteractive"
+			dangerouslySetInnerHTML={{
+				__html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PL6D4ZPL');
+        `,
+			}}
+		/>
 			</head>
 			<body className={'antialiased'}>
+				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PL6D4ZPL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				<Clarity />
 				<SanityLive />
 				{children}
